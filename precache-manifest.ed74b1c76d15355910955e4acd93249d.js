613883656e3230158e599656305e4551b7c57ded
Tarun Thota <tarunthota3@gmail.com>
2020-08-03 22:50:28 -0400

    Updates

---
+++ precache-manifest.ed74b1c76d15355910955e4acd93249d.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa445074f6b28a5825c515fdbe7673e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "07e4bc6309cdd8e4f98a",
+    "url": "/portfolio/static/css/2.f207efe9.chunk.css"
+  },
+  {
+    "revision": "fdda13e85a638885cae1",
+    "url": "/portfolio/static/css/main.814cd2e2.chunk.css"
+  },
+  {
+    "revision": "07e4bc6309cdd8e4f98a",
+    "url": "/portfolio/static/js/2.e3ef438d.chunk.js"
+  },
+  {
+    "revision": "0651e80d43008570f35594126b1824fd",
+    "url": "/portfolio/static/js/2.e3ef438d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdda13e85a638885cae1",
+    "url": "/portfolio/static/js/main.346c0161.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "91d8ff8ac25f51073a577408ae9cec86",
+    "url": "/portfolio/static/media/AvenirNextLTPro-Bold.91d8ff8a.otf"
+  },
+  {
+    "revision": "06248f3342288d7681fc55e2ffd7cd17",
+    "url": "/portfolio/static/media/AvenirNextLTPro-It.06248f33.otf"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/portfolio/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "944c30eff2d0ce7f67f5d8471f9fba75",
+    "url": "/portfolio/static/media/WMS.944c30ef.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "abb30ba018ce584ff0bb541022cd5f65",
+    "url": "/portfolio/static/media/imageedit_1_3658399494.abb30ba0.jpg"
+  },
+  {
+    "revision": "56fa3fe9682b572045731655afac44a2",
+    "url": "/portfolio/static/media/login.56fa3fe9.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "66908dca6a6078776a1f9b325c6d5fc5",
+    "url": "/portfolio/static/media/planning.66908dca.jpg"
+  },
+  {
+    "revision": "7053ba56dfa1f72f519d5e2d2a2475a2",
+    "url": "/portfolio/static/media/quiz.7053ba56.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "58c64c7c0037d771d783aa5e561bc359",
+    "url": "/portfolio/static/media/tarun_thota.58c64c7c.jpg"
+  },
+  {
+    "revision": "2b205f3f9231abe5f2e6cc4750a0c090",
+    "url": "/portfolio/static/media/weather.2b205f3f.jpg"
+  },
+  {
+    "revision": "6e1faefb4b9dc4632f0b9a08f94d58ce",
+    "url": "/portfolio/static/media/zynla.6e1faefb.jpg"
+  }
+]);
